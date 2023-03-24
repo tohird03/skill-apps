66042af76335ad5e59da04a042d6ff6d9a0c51d7
tohird03 <tohirjondoniyorov@gmail.com>
2023-03-24 15:00:47 +0500

    Add MiniDrawer sidebar

--- ts-redux-mui/src/App.tsx
+++ ts-redux-mui/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MiniDrawer from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      
+      <MiniDrawer/>
     </div>
   );
 }
